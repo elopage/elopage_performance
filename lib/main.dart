8ab8495881f002f7e37e276e99707e99c6cc005c
Bohdan Krokhmaliuk <bohdan.krokhmaliuk@elopage.com>
2022-09-08 12:18:45 +0300

    icons + style update

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,22 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'src/service_locator.dart';
 
+const MaterialColor elopageGreen = MaterialColor(
+  0xff7afba2,
+  <int, Color>{
+    50: Color(0xffb6fdcc),
+    100: Color(0xffaffdc7),
+    200: Color(0xffa2fcbe),
+    300: Color(0xff95fcb5),
+    400: Color(0xff87fbab),
+    500: Color(0xff7afba2),
+    600: Color(0xff6ee292),
+    700: Color(0xff62c982),
+    800: Color(0xff55b071),
+    900: Color(0xff499761),
+  },
+);
+
 void main() async {
   await Hive.initFlutter();
   serviceLocator.initializeDependencies();
@@ -16,7 +32,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final theme = ThemeData(primarySwatch: Colors.teal, useMaterial3: true);
+    final theme = ThemeData(primarySwatch: elopageGreen, useMaterial3: true);
     return MaterialApp(
       home: const ConfigurationsPage(),
       title: 'elopage performance',
